20f5a4645b31d596c3a71865a0b7f5dd08c5f271
Evellin Moura <126713662+evellinmoura@users.noreply.github.com>
2025-12-16 23:59:51 -0300

    Add docstring for node removal and balancing
    Add docstring explaining node removal and balancing.

---
+++ exercicio6.py
@@ -0,0 +1,10 @@
+"""
+        48
+       /  \
+     30    58
+    /  \
+  24   40
+Remove-se o nó 62
+A retirada provoca o desbalanceamento
+Aplica a rotação simples ou dupla, conforme o fator de balanceamento
+"""
