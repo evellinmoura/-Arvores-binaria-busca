d6a414f4329594170f55e02c1bc15177416dd926
Evellin Moura <126713662+evellinmoura@users.noreply.github.com>
2025-12-17 00:00:12 -0300

    Document red-black tree properties in exercicio7.py
    Add a description of a red-black tree structure.

---
+++ exercicio7.py
@@ -0,0 +1,17 @@
+"""
+sequencia de chaves:(5, 16, 22, 45, 2, 10, 18, 30, 50, 12, 1)
+(B) = preto
+(R) = vermelho
+               16(B)
+            /           \
+        5(R)             22(R)
+       /    \           /      \
+   2(B)   10(B)     18(B)     45(B)
+   /          \                 /    \
+1(R)         12(R)           30(R)  50(R)
+
+Raiz preta
+Nenhum nó vermelho tem filho vermelho
+Mesmo numero de nós pretos em todos os caminhos
+Balanceamento garantido
+"""
